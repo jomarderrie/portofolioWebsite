93ad2eb68c57a3946d9caabcd981344a8e68dc0b
NathanGanesh <deeznutsgottem6969@gmail.com>
2021-09-27 23:51:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import homeImage from './assets/homepageimage.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="main-nav">
+                <div>
+
+                </div>
+                <div className="socials">
+                    <a href="https://github.com/NathanGanesh" title="Github">
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href="mailto: nathangemieee@gmail.com" title="Mail">
+                        <i class="far fa-envelope"></i>
+                    </a>
+                    <a
+                        href="https://nl.linkedin.com/in/nathan-ganesh-606969168"
+                        title="LinkedIn"
+                    >
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a href="" title="Resume">
+                        <i class="far fa-id-card"></i>
+                    </a>
+                </div>
+                <div className="header-sections">
+                    <p>Home</p>
+                    <p>Projects</p>
+                    <p>Resume</p>
+                </div>
+            </nav>
+
+
+            <main className="main">
+                <div className="main-home">
+                    <div className="home-image">
+                        <img src={homeImage}/>
+                    </div>
+                    <div className={"main-info"} style={{margin: '20px'}}>
+                        <div style={{fontSize: '50px'}}>Hi i'm <span style={{color: '#1D3461',}}>Nathan</span></div>
+                        <div>
+                            <p style={{fontSize: '20px'}}>
+                                Third year student software engineering at Saxion. Web developer having an experience of
+                                building web applications with <span style={{color: 'orange'}}>Javascript</span>,<span
+                                style={{color: '#1F487E'}}> Reactjs</span>, <span
+                                style={{color: '#FF7F7F'}}> Angularjs</span>,
+                                <span style={{color: '#006400'}}> Nodejs</span> and <span
+                                style={{color: 'red'}}>Java</span>.
+                            </p>
+                        </div>
+                        <div style={{padding: '20px 0px', fontSize: '18px'}}>
+                            <button><i className="fa fa-file" style={{marginRight: '.25rem'}}></i>See my resume</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={'main-project'}>
+                    <div style={{fontSize: '22px'}}>
+                        <h3>A few projects</h3></div>
+                    <div style={{fontSize: '16px'}}>
+                        Here is a list of projects i have done on my personal time or for
+                        studies
+                    </div>
+                </div>
+                <div className={'project-items'}>
+
+                    <div className={'categories'}>
+
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/assets/projects.js
@@ -0,0 +1,55 @@
+export default [{
+    name: 'React product techstore',
+    categories: ["Reactjs", "Node", "Mongo", "Redux", "Aws"],
+    github: 'https://github.com/NathanGanesh/producttechreduxecomstore',
+    description: 'A full featured product techstore with admin functionality and checkout functionality',
+    image: 'https://camo.githubusercontent.com/5c7a81e6ac67feac240aed1296198b5694414b01fa7d8236fe5e4744878bc945/68747470733a2f2f692e696d6775722e636f6d2f4c4530306e50532e706e67',
+    link: '',
+},
+    //compiler
+    {
+        name: 'Compilers and Operating systems',
+        categories: ["Java", "School"],
+        github: 'https://github.com/NathanGanesh/compilerAndOperatingSystems',
+        description: 'Made my own programming language called germava.',
+        image: 'https://github.com/NathanGanesh/compilerAndOperatingSystems/blob/main/doc/readme/compiler-phases.png',
+        link: 'https://github.com/NathanGanesh/compilerAndOperatingSystems',
+    },
+    //imdb react
+    {
+        name: 'React cinema',
+        categories: ["ci/cd", "reactjs", "github"],
+        github: 'https://github.com/NathanGanesh/reactcinema',
+        description: 'An cinema search engine based with CI/CD functionality',
+        image: 'https://user-images.githubusercontent.com/64851813/134892598-5a45a08b-c8a3-4305-9890-c154a0cd5806.png',
+        link: 'http://therealdealimdbreact.s3-website-us-east-1.amazonaws.com',
+    },
+//taskworklife personal angular project
+    {
+        name: "Task work life",
+        categories: ["Personal project", "Spring", "Angular"],
+        github: "https://github.com/NathanGanesh/taskworklifeBackend",
+        description: "Personal project to manage your dailies and tasks in life",
+        image: "https://user-images.githubusercontent.com/64851813/134957215-b70ec256-add3-481a-9a50-07a924d69e13.png",
+        link: "See github for screenshots",
+    },
+//stockmarket inprogress
+    {
+        name: "Stock market",
+        categories: ["Spring boot", "Angular", "In progress"],
+        github: ["https://github.com/NathanGanesh/stockmarket"],
+        description: "Application to track stocks and trend algorithms.",
+        image: "https://imgur.com/a/L1DaSU3",
+        link: "See github",
+    },
+//algo en data structs in progress
+    {
+        name: "Algo and data structs",
+        categories: ["java", "In progress"],
+        github: "https://github.com/NathanGanesh/Algodatastructs",
+        description: "Implementing a diverse array of algorithms.",
+        image: "https://imgur.com/a/eR5EbHM",
+        link: "See github",
+    },
+
+]
